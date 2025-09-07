6c4493b4fb6150a3a01302c0395a059d557af25f
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-09-07 02:09:26 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from './components/Navbar'
+
 const App = () => {
-  return <div className='px-4 md:px-8'>Hello World</div>
+  return (
+    <div className='px-4 md:px-8'>
+      <div>{Navbar}</div>
+      <div>{Navbar}</div>
+      <div>{Navbar}</div>
+      <div>{Navbar}</div>
+      <div>{Navbar}</div>
+    </div>
+  )
 }
 
 export default App
